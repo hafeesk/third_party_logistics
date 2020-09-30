39bb30c5ec4cac2630edecdce1d08cca54211976
ashish-greycube <mr.ashish.shah@gmail.com>
2020-09-30 13:41:01 +0200

    config file + custom fields doc

---
+++ third_party_logistics/config/third_party_logistics.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+
+def get_data():
+    config = [{'label': _('Transaction'), 'items': [{
+        'type': 'doctype',
+        'name': 'Sales Order',
+        'label': 'Sales Order',
+        'description': 'Sales Order'
+        },{
+        'type': 'doctype',
+        'name': 'Delivery Note',
+        'label': 'Delivery Note',
+        'description': 'Delivery Note'
+        },{
+        'type': 'doctype',
+        'name': 'Stock Entry',
+        'label': 'Stock Entry',
+        'description': 'Stock Entry'
+        },{
+        'type': 'doctype',
+        'name': 'Service Note CT',
+        'label': 'Service Note',
+        'description': 'Service Note'
+        },{
+        'type': 'doctype',
+        'name': 'Sales Invoice',
+        'label': 'Sales Invoice',
+        'description': 'Sales Invoice'
+        }]},{'label': _('Setup'), 'items': [{
+        'type': 'doctype',
+        'name': 'Customer',
+        'label': 'Customer',
+        'description': 'Customer'
+        },{
+        'type': 'doctype',
+        'name': 'Third Party Logistics Settings',
+        'label': 'Third Party Logistics Settings',
+        'description': 'Third Party Logistics Settings'
+        },{
+        'type': 'doctype',
+        'name': 'Storage Charge Log CT',
+        'label': 'Storage Charge Log CT',
+        'description': 'Storage Charge Log CT'
+        }]},{'label': _('Reports'), 'items': [{
+        'type': 'report',
+        'name': 'Storage Billing Report',
+        'label': 'Storage Billing Report',
+	    "is_query_report": True,
+	    'doctype':'Storage Charge Log CT',
+        'description': 'Storage Charge Log CT'
+        }]}]
+
+    return config
